ccea69b6b4692f1424003b6727ccabd72000bb5d
Eugene Evstafev <ee345@cam.ac.uk>
2025-12-04 15:31:40 +0000

    feat: add complex and check tables with data handling

---
+++ src/views/admin/apiKeys/index.jsx
@@ -0,0 +1,20 @@
+import {
+  columnsDataComplex,
+} from "./variables/columnsData";
+import tableDataComplex from "./variables/tableDataComplex.json";
+import ComplexTable from "./components/ComplexTable";
+
+const ApiKeys = () => {
+  return (
+    <div>
+      <div className="mt-5 grid h-full grid-cols-1 gap-5">
+        <ComplexTable
+          columnsData={columnsDataComplex}
+          tableData={tableDataComplex}
+        />
+      </div>
+    </div>
+  );
+};
+
+export default ApiKeys;
